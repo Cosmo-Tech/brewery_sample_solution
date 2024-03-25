571bb7682f994f3f7136cdd4060e19240a85ad2b
Tristan Huet <tristan.huet@cosmotech.com>
2024-03-25 19:53:23 +0100

    feat: add new ETL to create subdatasets

---
+++ code/run_templates/etl_sub_dataset_by_filter/create_subdataset.py
@@ -0,0 +1,70 @@
+import cosmotech_api
+from cosmotech_api.model.sub_dataset_graph_query import SubDatasetGraphQuery
+
+import common
+
+
+LOGGER = common.get_logger()
+
+
+def load_env_variables():
+    from dotenv import load_dotenv
+    load_dotenv()
+    # Example of .env file required to run this script
+    # CSM_API_URL="https://dev.api.cosmotech.com/phoenix/v3-0"
+    # CSM_API_SCOPE="http://dev.api.cosmotech.com/.default"
+    # AZURE_TENANT_ID="<insert your tenant id here>"
+    # AZURE_CLIENT_ID="<insert your client id here>"
+    # AZURE_CLIENT_SECRET="<insert the client secret here>"
+    # CSM_ORGANIZATION_ID="<insert your organization id here>"
+    # CSM_WORKSPACE_ID="<insert your workspace id here>"
+
+
+def parse_arguments():
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Create a subdataset from a parent dataset and a list of filter arguments.'
+    )
+    parser.add_argument("-o", "--organization", help="Organization id")
+    parser.add_argument("-p", "--parent", help="Id of the parent dataset")
+    parser.add_argument("-n", "--name", help="Name of the created subdataset")
+    parser.add_argument("-d", "--description", help="Description of the created subdataset")
+    return parser.parse_args()
+
+
+def create_subdataset(organization_id, parent_dataset_id, subdataset_details):
+    api = common.get_api()
+
+    try:
+        parent_dataset = api['dataset'].find_dataset_by_id(organization_id, parent_dataset_id)
+    except cosmotech_api.ApiException as e:
+        LOGGER.error(f"Failed to retrieve parent dataset with id {parent_dataset_id}: %s\n" % e)
+        raise e
+
+    LOGGER.info("Preparing creation of the subdataset...")
+    parent_dataset_name = parent_dataset['name'] or ''
+    parent_dataset_description = parent_dataset['description'] or parent_dataset_name  # Description filler
+    subdataset_graph_query = SubDatasetGraphQuery(
+        name=subdataset_details['name'] or f'Subdataset of {parent_dataset_name}',
+        description=subdataset_details['description'] or f'(Subdataset) {parent_dataset_description}',
+        queries=["MATCH (n) RETURN n"],
+        main=True,
+    )
+
+    LOGGER.info("Creating subdataset...")
+    try:
+        subdataset = api['dataset'].create_sub_dataset(organization_id, parent_dataset_id, subdataset_graph_query)
+    except cosmotech_api.ApiException as e:
+        LOGGER.error("Failed to create subdataset: %s\n" % e)
+        raise e
+
+    LOGGER.info(f"Subdataset ready! ({subdataset['id']})")
+    return subdataset
+
+
+if __name__ == "__main__":
+    # Only when running locally: load .env file & parse script parameters
+    load_env_variables()
+    args = parse_arguments()
+    subdataset_details = {"name": args.name, "description": args.description}
+    dataset = create_subdataset(args.organization, args.parent, subdataset_details)
---
+++ code/run_templates/etl_sub_dataset_by_filter/etl.py
@@ -0,0 +1,37 @@
+# import json
+import os
+import sys
+
+import common
+from create_subdataset import create_subdataset
+
+
+LOGGER = common.get_logger()
+
+
+def main():
+    LOGGER.info("Starting the ETL Run")
+    organization_id = os.environ.get("CSM_ORGANIZATION_ID")
+    workspace_id = os.environ.get("CSM_WORKSPACE_ID")
+    runner_id = os.environ.get("CSM_RUNNER_ID")
+
+    api = common.get_api()
+    runner = api.runner.get_runner(organization_id=organization_id, workspace_id=workspace_id, runner_id=runner_id)
+    parent_dataset_id = runner['dataset_list'][0]
+
+    # LOGGER.info("Loading parameters")
+    # with open(os.path.join(os.environ.get("CSM_PARAMETERS_ABSOLUTE_PATH"), "parameters.json")) as f:
+    #     parameters = {d["parameterId"]: d["value"] for d in json.loads(f.read())}
+
+    create_subdataset(organization_id, parent_dataset_id)
+    # subdataset = create_subdataset(organization_id, parent_dataset_id, subdataset_details) # TODO
+
+    LOGGER.info("ETL Run finished")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        LOGGER.error("An error occured during the creation of the sub-dataset: %s\n" % e)
+        sys.exit(-1)

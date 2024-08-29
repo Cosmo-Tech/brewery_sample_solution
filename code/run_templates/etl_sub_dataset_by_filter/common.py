ab566048418de9d1dc3f13f95545008264d93a6f
Tristan Huet <tristan.huet@cosmotech.com>
2024-08-29 16:48:11 +0200

    feat: add API_KEY support in ETL run templates

--- code/run_templates/etl_sub_dataset_by_filter/common.py
+++ code/run_templates/etl_sub_dataset_by_filter/common.py
@@ -2,10 +2,10 @@ import os
 import logging
 from rich.logging import RichHandler
 
-import cosmotech_api
 from azure.identity import DefaultAzureCredential
 from cosmotech_api.api.dataset_api import DatasetApi
 from cosmotech_api.api.runner_api import RunnerApi
+from cosmotech.coal.cosmotech_api.connection import get_api_client
 
 
 LOGGER = logging.getLogger("my_etl_logger")
@@ -22,16 +22,11 @@ def get_logger():
 
 
 def get_api():
-    credentials = DefaultAzureCredential()
-    configuration = cosmotech_api.Configuration(
-        host=os.environ.get("CSM_API_URL"),
-        discard_unknown_keys=True,
-        access_token=credentials.get_token(os.environ.get("CSM_API_SCOPE")).token,
-    )
+    api_client, api_client_name = get_api_client()
+    LOGGER.info(f"Using API client '{api_client_name}'")
 
-    with cosmotech_api.ApiClient(configuration) as api_client:
-        dataset_api_instance = DatasetApi(api_client)
-        runner_api_instance = RunnerApi(api_client)
+    dataset_api_instance = DatasetApi(api_client)
+    runner_api_instance = RunnerApi(api_client)
 
     return {"dataset": dataset_api_instance, "runner": runner_api_instance}
 
@@ -39,3 +34,18 @@ def get_api():
 def get_api_token():
     credentials = DefaultAzureCredential()
     return credentials.get_token(os.environ.get("CSM_API_SCOPE")).token
+
+
+def get_authentication_header():
+    api_client, api_client_name = get_api_client()
+    api_key = os.environ.get("CSM_API_KEY")
+
+    if api_key is not None:
+        # Craft header with API key
+        header_name = os.environ.get("CSM_API_KEY_HEADER", "X-CSM-API-KEY")
+        return {header_name: api_key}
+
+    # Craft header for Bearer token
+    token = get_api_token()
+    auth_value = f"Bearer {token}"
+    return {"Authorization": auth_value}

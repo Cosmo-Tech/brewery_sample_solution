7cef8fba548899a32ddda7d3019efac061c99ae7
Tristan Huet <tristan.huet@cosmotech.com>
2024-08-30 18:42:00 +0200

    refactor: coding style & refactoring to use common functions in all ETLs

--- code/run_templates/etl_sub_dataset_by_filter_multi/etl.py
+++ code/run_templates/etl_sub_dataset_by_filter_multi/etl.py
@@ -3,17 +3,13 @@ import shutil
 import os
 import sys
 import cosmotech_api
-from cosmotech_api.model.dataset import Dataset
-from pathlib import Path
 
-sys.path.append(str(Path(__file__).parents[1] / "etl_sub_dataset_by_filter"))
-
-from twingraph import (
+from common.common import get_logger, get_api
+from common.twingraph import (
     parse_twingraph_json,
     create_csv_files_from_graph_content,
     upload_twingraph_zip_archive,
 )
-from common import get_logger, get_api
 
 LOGGER = get_logger()
 
@@ -25,9 +21,7 @@ def main():
     runner_id = os.environ.get("CSM_RUNNER_ID")
     api = get_api()
 
-    runner = api["runner"].get_runner(
-        organization_id=organization_id, workspace_id=workspace_id, runner_id=runner_id
-    )
+    runner = api["runner"].get_runner(organization_id=organization_id, workspace_id=workspace_id, runner_id=runner_id)
     # A webapp convention is that the first dataset is always the subdataset and
     # the second the parent dataset, see doc at:
     # https://github.com/Cosmo-Tech/azure-sample-webapp/blob/main/doc/datasetManager.md#subdataset-creation-scripts
@@ -50,7 +44,12 @@ def main():
 
 
 def etl_sub_dataset_by_filter_multi(
-    api, organization_id, parent_dataset_id, subdataset_id, workspace_id, graph_filter_list
+    api,
+    organization_id,
+    parent_dataset_id,
+    subdataset_id,
+    workspace_id,
+    graph_filter_list,
 ):
     LOGGER.info("Linking subdataset to workspace...")
     try:
@@ -60,23 +59,21 @@ def etl_sub_dataset_by_filter_multi(
         raise e
     LOGGER.info("Subdataset linked and ready!")
     LOGGER.info("Query and filter dataset")
-# get nodes without label :Customer OR nodes with label:Customer but with their property id is in the list X return as n
-# get all edges where source AND target nodes do not have the label Customer or
-# if they do their property `id` is not in list X
+    # get nodes without label :Customer OR nodes with label:Customer but with their property id is in the list X return
+    # as n
+    # get all edges where source AND target nodes do not have the label Customer or
+    # if they do their property `id` is not in list X
     node_query = [f"MATCH (n) WHERE ( (NOT n:Customer) OR (n.id IN {graph_filter_list}) ) RETURN n"]
-    edge_query = [f"MATCH (src)-[edge]->(dst) WHERE "
-                 f"( (NOT src:Customer) OR (src.id IN {graph_filter_list}) ) "
-                  "AND"
-                 f"( (NOT dst:Customer) OR (dst.id IN {graph_filter_list}) ) "
-                  "RETURN src, edge, dst"]
+    edge_query = [
+        f"MATCH (src)-[edge]->(dst) WHERE "
+        f"( (NOT src:Customer) OR (src.id IN {graph_filter_list}) ) "
+        "AND"
+        f"( (NOT dst:Customer) OR (dst.id IN {graph_filter_list}) ) "
+        "RETURN src, edge, dst"
+    ]
 
-
-    nodes = api["dataset"].twingraph_query(
-        organization_id, parent_dataset_id, node_query
-    )
-    edges = api["dataset"].twingraph_query(
-        organization_id, parent_dataset_id, edge_query
-    )
+    nodes = api["dataset"].twingraph_query(organization_id, parent_dataset_id, node_query)
+    edges = api["dataset"].twingraph_query(organization_id, parent_dataset_id, edge_query)
     LOGGER.info("Transform JSON to csv")
     graph_content = parse_twingraph_json(nodes, edges, "n", "edge", "src", "dst")
     create_csv_files_from_graph_content(graph_content, "twingraph_dump")

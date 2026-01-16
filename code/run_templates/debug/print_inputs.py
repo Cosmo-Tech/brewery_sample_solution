f19afbbd0b4aa0483a4cd0ad89e67ed466b9a135
Laurent Alépée <laurent.alepee@cosmotech.com>
2026-01-16 16:19:11 +0100

    add better debug printing

--- code/run_templates/debug/print_inputs.py
+++ code/run_templates/debug/print_inputs.py
@@ -1,23 +1,55 @@
-import os
 from cosmotech.coal.utils.configuration import Configuration
 from pathlib import Path
+
+
+# Source - https://stackoverflow.com/a
+# Posted by Aaron Hall, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-01-15, License - CC BY-SA 4.0
+
+
+def tree(dir_path: Path, prefix: str = ''):
+    """A recursive generator, given a directory Path object
+    will yield a visual tree structure line by line
+    with each line prefixed by the same characters
+    """
+    # prefix components:
+    space =  '    '
+    branch = '│   '
+    # pointers:
+    tee =    '├── '
+    last =   '└── '
+
+    contents = list(dir_path.iterdir())
+    # contents each get pointers that are ├── with a final └── :
+    pointers = [tee] * (len(contents) - 1) + [last]
+    for pointer, path in zip(pointers, contents):
+        size = ""
+        if path.is_file():
+            size = f": {path.stat().st_size}o"
+        yield prefix + pointer + path.name + size
+        if path.is_dir():  # extend the prefix and recurse:
+            extension = branch if pointer == tee else space
+            # i.e. space because last, └── , above so no more |
+            yield from tree(path, prefix=prefix+extension)
+
+
+def dprint(line):
+    pre = '[debug]'
+    print(f"{pre} {line}")
 
 
 def main():
     _conf = Configuration()
 
     param_path = Path(_conf.cosmotech.parameters_absolute_path)
-    print(f"print {param_path.resolve()} content:")
-    pf = os.listdir(param_path)
-    print(pf)
+    dprint(f"Printing {param_path.resolve()} content:")
+    for line in tree(param_path):
+        dprint(line)
 
     data_path = Path(_conf.cosmotech.dataset_absolute_path)
-    print(f"print {data_path.resolve()} content:")
-    df = os.listdir(data_path)
-    print(df)
-    for f in df:
-        print(f)
-        print((param_path / f).stat().st_size)
+    dprint(f"Printng {data_path.resolve()} content:")
+    for line in tree(data_path):
+        dprint(line)
 
 
 if __name__ == "__main__":

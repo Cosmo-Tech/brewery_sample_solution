61090cdb835dd2299ecb20998540df0691880de7
Laurent Alépée <laurent.alepee@cosmotech.com>
2026-02-17 15:30:31 +0100

    fix: use Path class in fetcher

--- code/run_templates/common/fetcher.py
+++ code/run_templates/common/fetcher.py
@@ -5,14 +5,14 @@ from cosmotech.coal.utils.configuration import ENVIRONMENT_CONFIGURATION as EC
 
 
 def fetch_dataset_file_path(dataset_name: str, dataset_id: str = "") -> Path:
-    for r, d, f in os.walk(EC.cosmotech.dataset_absolute_path / dataset_id):
+    for r, d, f in os.walk(Path(EC.cosmotech.dataset_absolute_path) / dataset_id):
         if dataset_name in f:
             return Path(r) / dataset_name
     raise FileNotFoundError(f"File for {dataset_name} not found in {EC.cosmotech.dataset_absolute_path}.")
 
 
 def fetch_parameter_file_path(param_name: str) -> Path:
-    for r, d, f in os.walk(EC.cosmotech.parameters_absolute_path):
+    for r, d, f in os.walk(Path(EC.cosmotech.parameters_absolute_path)):
         if param_name in r:
             return Path(r) / f[0]
     raise FileNotFoundError(f"File for {param_name} not found in {EC.cosmotech.parameters_absolute_path}.")
